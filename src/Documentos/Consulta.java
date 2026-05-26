aee0506cbf67723670e99e2669fed3e498bd67ed
⋆｡°✩ 𝔊𝔞𝔟𝔯𝔦𝔢𝔩 𝔒𝔯𝔱𝔢𝔤𝔞 ✩°｡⋆ <hortegagabriel73@gmail.com>
2026-05-26 09:51:41 -0300

    Enhance documentation for Consulta class

--- src/Documentos/Consulta.java
+++ src/Documentos/Consulta.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Documentos;
 
 import Entidades.Medico;
@@ -10,7 +7,17 @@ import java.util.Scanner;
 
 /**
  *
- * @author guest-kt6far
+ * Classe responsável por representar uma consulta médica no sistema.
+ * 
+ * Cada consulta possui:
+ * - paciente
+ * - médico
+ * - data
+ * - horário
+ * - tipo da consulta
+ * 
+ * O tipo pode representar:
+ * consulta normal ou retorno.
  */
 public class Consulta {
     public Scanner sc = new Scanner(System.in);
@@ -20,6 +27,15 @@ public class Consulta {
     private Medico m;
     private String tipo; //(consulta normal: duração de 1 hora, retorno: duração de 30 minutos.)
 
+    /**
+     * Construtor da classe Consulta.
+     * 
+     * @param p paciente da consulta
+     * @param data data da consulta
+     * @param horario horário da consulta
+     * @param m médico responsável
+     * @param tipo tipo da consulta
+     */
     public Consulta(Paciente p, String data, String horario, Medico m, String tipo) {
         this.p = p;
         this.data = data;
@@ -27,7 +43,7 @@ public class Consulta {
         this.m = m;
         this.tipo = tipo;
     }
-
+    // metodos gets e sets dos atributos da classe
     public String getHorario (){
         return horario;
     }
@@ -40,7 +56,7 @@ public class Consulta {
     public String toString() {
         return "Consulta -> Data: " + data + " | Horário: " + horario + " | Paciente: " + p.getNome() + " | Médico: " + m.getNome();
     }
-
+    //  Define ou altera a data da consulta.
     public void setData(String data) {
         this.data = data;
     }
